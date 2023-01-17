9c73555c868aac491ef827786acdb0ccc723ea29
hayktermarkosyan <hayktermarkosyan@gmail.com>
2023-01-17 10:42:27 +0400

    17/01/23-3

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import './Home.css';
 
 function Home() {
     return (
-        <div className="container mb-5">
+        <div className="container mb-5" style={{ marginTop: 250}}>
             <hr/>
             <div className="col-md-8 offset-md-2">
                 <p>
